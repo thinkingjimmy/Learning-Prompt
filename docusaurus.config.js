8e238343cda9e56b89f82c622d84d739a2cf3043
thinkingjimmy <thinkingjimmy@outlook.com>
2023-03-06 23:40:08 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'thinkingjimmy',
-
-  projectName: 'Learning-Prompt',
+  projectName: 'Learning-prompt.github.io',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
