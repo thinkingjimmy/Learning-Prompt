75a68088cd6d758892a0db6acb05e1fb691839c3
thinkingjimmy <thinkingjimmy@outlook.com>
2023-03-07 10:05:57 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   organizationName: 'thinkingjimmy',
   projectName: 'Learning-prompt',
   trailingSlash: false,
-  //deploymentBranch: 'main',
+  deploymentBranch: 'main',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
